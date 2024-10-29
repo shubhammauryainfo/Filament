e16fb9a3ac23f96336c562df2e61eedce98e82b4
shubhammauryainfo <shubhammaurya8828@gmail.com>
2024-10-29 15:48:32 +0530

    some migration

---
+++ app/Models/ListingParty.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ListingParty extends Model
+{
+    /** @use HasFactory<\Database\Factories\ListingPartyFactory> */
+    use HasFactory;
+
+}
---
+++ app/Models/episodes.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class episodes extends Model
+{
+    /** @use HasFactory<\Database\Factories\EpisodesFactory> */
+    use HasFactory;
+}
